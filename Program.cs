9e53e6ed3b99bdb302a4ec941584492eebef1a4c
rudyyazdi <rudyyazdi@gmail.com>
2016-08-14 20:49:00 +1000

    concatttt

--- Program.cs
+++ Program.cs
@@ -8,7 +8,12 @@ namespace ConsoleApplication
     {
       string[] words = Program.Read("data/names.txt");
       Array.Sort(words);
+      char[] test = "woorrd".ToCharArray();
       Console.WriteLine(Program.TextValue("worddd"));
+      int[] con1 = {1};
+      int[] con2 = {1};
+      int[] con3 = Program.Concat(con1, con2);
+      foreach(int w in con3) Console.WriteLine(w);
     }
     private static int TextValue(string word)
     {
@@ -18,6 +23,13 @@ namespace ConsoleApplication
     {
       string data = System.IO.File.ReadAllText(path);
       return data.Split(',');
+    }
+    public static T[] Concat<T>(T[] x, T[] y)
+    {
+      int oldLen = x.Length;
+      Array.Resize<T>(ref x, x.Length + y.Length);
+      Array.Copy(y, 0, x, oldLen, y.Length);
+      return x;
     }
   }
 }
