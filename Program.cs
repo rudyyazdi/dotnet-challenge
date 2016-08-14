1bb2d6d077a10267d7e52fc30a79f30cece28fce
rudyyazdi <rudyyazdi@gmail.com>
2016-08-14 19:39:04 +1000

    sorted the array finally

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@ namespace ConsoleApplication
   public class Program
   {
     public static void Main(string[] args)
-    {
-      var data = System.IO.File.ReadAllText("data/names.txt");
-      var words = data.Split(',');
-      foreach (var word in words)
-         Console.WriteLine(word);
+        {
+            string data = System.IO.File.ReadAllText("data/names.txt");
+            string[] words = data.Split(',');
+            Array.Sort(words);
+            foreach (var word in words) Console.WriteLine(word);
+        }
     }
-  }
 }
